3967dc3386951377cc1a8db542d76ecb4ed335b1
enotvtapke <30431325+enotvtapke@users.noreply.github.com>
2019-02-23 20:05:53 +0300

    1

--- websock.py
+++ websock.py
@@ -502,7 +502,7 @@ def ondisconnect(self, clientName, address):
 
 
 
-sigServ = WebSocketServer("0.0.0.0", 8001, 4, **{"onmessage": onmessage, "onconnect": onconnect, "ondisconnect": ondisconnect})
+sigServ = WebSocketServer("0.0.0.0", (process.env.PORT || 4000), 4, **{"onmessage": onmessage, "onconnect": onconnect, "ondisconnect": ondisconnect})
 
 a_loop = asyncio.get_event_loop()
 a_loop.run_until_complete(sigServ.start())
