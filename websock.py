09208cb990c625716a53c531e331cc3639562abd
enotvtapke <30431325+enotvtapke@users.noreply.github.com>
2019-02-27 17:35:21 +0300

    1

--- websock.py
+++ websock.py
@@ -215,8 +215,9 @@ class WebSocketServer:
     magic = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" 
     magic = self.args.get('magic', magic) 
     header = str(await a_loop.sock_recv(client, 1000))
+    print(header)
     try: 
-      res = header.index("Sec-Websocket-Key")
+      res = header.index("Sec-webSocket-Key")
     except ValueError:
       print("Value error")
       return False
